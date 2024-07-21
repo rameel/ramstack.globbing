02c9b3d8facf62bd2f6bccfed39ae10803a39244
rameel <rameel-b@hotmail.com>
2024-07-21 11:01:32 +0500

    Formatting

--- Ramstack.Globbing.Tests/Traversal/FilesTests.cs
+++ Ramstack.Globbing.Tests/Traversal/FilesTests.cs
@@ -16,23 +16,37 @@ public class FilesTests
     [Test]
     public void EnumerateFiles_Default_IncludesHidden()
     {
-        var count = Files.EnumerateFiles(_storage.Root, "**").Count();
+        var count = Files
+            .EnumerateFiles(_storage.Root, "**")
+            .Count();
+
         Assert.That(count, Is.EqualTo(_storage.FileList.Count));
     }
 
     [Test]
     public void EnumerateDirectories_Default_IncludesHidden()
     {
-        var count = Files.EnumerateDirectories(_storage.Root, "**").Count();
-        var expected = Directory.EnumerateDirectories(_storage.Root, "*", SearchOption.AllDirectories).Count();
+        var count = Files
+            .EnumerateDirectories(_storage.Root, "**")
+            .Count();
+
+        var expected = Directory
+            .EnumerateDirectories(_storage.Root, "*", SearchOption.AllDirectories)
+            .Count();
+
         Assert.That(count, Is.EqualTo(expected));
     }
 
     [Test]
     public void EnumerateFiles_OneLevel()
     {
-        var list = Files.EnumerateFiles(_storage.Root, "project/*").OrderBy(p => p);
-        var expected = Directory.EnumerateFiles(Path.Combine(_storage.Root, "project")).OrderBy(p => p);
+        var list = Files
+            .EnumerateFiles(_storage.Root, "project/*")
+            .OrderBy(p => p);
+
+        var expected = Directory
+            .EnumerateFiles(Path.Combine(_storage.Root, "project"))
+            .OrderBy(p => p);
 
         Assert.That(list, Is.EquivalentTo(expected));
     }
@@ -40,8 +54,13 @@ public class FilesTests
     [Test]
     public void EnumerateDirectories_OneLevel()
     {
-        var list = Files.EnumerateDirectories(_storage.Root, "project/*").OrderBy(p => p);
-        var expected = Directory.EnumerateDirectories(Path.Combine(_storage.Root, "project")).OrderBy(p => p);
+        var list = Files
+            .EnumerateDirectories(_storage.Root, "project/*")
+            .OrderBy(p => p);
+
+        var expected = Directory
+            .EnumerateDirectories(Path.Combine(_storage.Root, "project"))
+            .OrderBy(p => p);
 
         Assert.That(list, Is.EquivalentTo(expected));
     }
@@ -62,8 +81,13 @@ public class FilesTests
     [TestCase("vb")]
     public void EnumerateFiles_ByExtension(string ext)
     {
-        var list = Files.EnumerateFiles(_storage.Root, $"**/*.{ext}").OrderBy(p => p);
-        var expected = Directory.EnumerateFiles(_storage.Root, $"*.{ext}", SearchOption.AllDirectories).OrderBy(p => p);
+        var list = Files
+            .EnumerateFiles(_storage.Root, $"**/*.{ext}")
+            .OrderBy(p => p);
+
+        var expected = Directory
+            .EnumerateFiles(_storage.Root, $"*.{ext}", SearchOption.AllDirectories)
+            .OrderBy(p => p);
 
         Assert.That(list, Is.EquivalentTo(expected));
     }
@@ -71,7 +95,10 @@ public class FilesTests
     [Test]
     public void EnumerateFiles_BraceExtension()
     {
-        var list = Files.EnumerateFiles(_storage.Root, "**/*.{log,dat}").OrderBy(p => p);
+        var list = Files
+            .EnumerateFiles(_storage.Root, "**/*.{log,dat}")
+            .OrderBy(p => p);
+
         var expected = Directory
             .EnumerateFiles(_storage.Root, "*", SearchOption.AllDirectories)
             .Where(p => Path.GetExtension(p) is ".log" or ".dat")
@@ -83,7 +110,10 @@ public class FilesTests
     [Test]
     public void EnumerateFiles_Set()
     {
-        var list = Files.EnumerateFiles(_storage.Root, "**/tests/[a-zA-Z]*/*.xml").OrderBy(p => p);
+        var list = Files
+            .EnumerateFiles(_storage.Root, "**/tests/[a-zA-Z]*/*.xml")
+            .OrderBy(p => p);
+
         var expected = Directory
             .EnumerateFiles(Path.Combine(_storage.Root, "project", "tests"), "*.xml", SearchOption.AllDirectories)
             .OrderBy(p => p);
@@ -94,7 +124,10 @@ public class FilesTests
     [Test]
     public void EnumerateFiles_Logs()
     {
-        var list = Files.EnumerateFiles(_storage.Root, "**/logs/**/*.log").OrderBy(p => p);
+        var list = Files
+            .EnumerateFiles(_storage.Root, "**/logs/**/*.log")
+            .OrderBy(p => p);
+
         var expected = Directory
             .EnumerateFiles(_storage.Root, "*.log", SearchOption.AllDirectories)
             .OrderBy(p => p);
@@ -105,7 +138,10 @@ public class FilesTests
     [Test]
     public void EnumerateFiles_Temps()
     {
-        var list = Files.EnumerateFiles(_storage.Root, "**/{hidden{,-folder}}/**/*.{tmp,dat}").OrderBy(p => p);
+        var list = Files
+            .EnumerateFiles(_storage.Root, "**/{hidden{,-folder}}/**/*.{tmp,dat}")
+            .OrderBy(p => p);
+
         var expected = Directory
             .EnumerateFiles(_storage.Root, "*", SearchOption.AllDirectories)
             .Where(p => Regex.IsMatch(p, @"\b(hidden|hidden-folder)\b"))
@@ -120,7 +156,10 @@ public class FilesTests
     [TestCase(@"project\**\{hidden{,-folder}}\**\*.{tmp,dat}")]
     public void EnumerateFiles_NoEscaping(string pattern)
     {
-        var list = Files.EnumerateFiles(_storage.Root, pattern, flags: MatchFlags.Windows).OrderBy(p => p);
+        var list = Files
+            .EnumerateFiles(_storage.Root, pattern, flags: MatchFlags.Windows)
+            .OrderBy(p => p);
+
         var expected = Directory
             .EnumerateFiles(_storage.Root, "*", SearchOption.AllDirectories)
             .Where(p => Regex.IsMatch(p, @"\b(hidden|hidden-folder)\b"))
