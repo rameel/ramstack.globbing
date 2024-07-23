08c2e957c10dc938d5f3578c029d9deeef9fba1a
rameel <rameel-b@hotmail.com>
2024-07-23 22:36:01 +0500

    Add tests

---
+++ Ramstack.Globbing.Tests/Utilities/PathHelperTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Ramstack.Globbing.Utilities;
+
+[TestFixture]
+public partial class PathHelperTests
+{
+    [TestCase("", 1)]
+    [TestCase("//", 1)]
+    [TestCase("/dir1", 1)]
+    [TestCase("dir1", 1)]
+    [TestCase("/dir1/dir2/", 2)]
+    [TestCase("dir1/dir2", 2)]
+    [TestCase("dir1/dir2/", 2)]
+    [TestCase("///dir1/dir2////", 2)]
+    public void CountPathSegmentsTests(string path, int expected)
+    {
+        Assert.That(
+            PathHelper.CountPathSegments(path, MatchFlags.Windows),
+            Is.EqualTo(expected));
+
+        Assert.That(
+            PathHelper.CountPathSegments(path, MatchFlags.Unix),
+            Is.EqualTo(expected));
+
+        Assert.That(
+            PathHelper.CountPathSegments(path.Replace('/', '\\'), MatchFlags.Windows),
+            Is.EqualTo(expected));
+    }
+}
