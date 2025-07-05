960b7757c5810e73661dc8cd84bc10a13bab1ca7
rameel <rameel-b@hotmail.com>
2025-07-05 20:24:35 +0500

    Test SIMD fallbacks by disabling AVX2/SSE2

---
+++ Ramstack.Globbing.Tests/SimdConfigurationTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Ramstack.Globbing;
+
+[TestFixture]
+public class SimdConfigurationTests
+{
+    [Test]
+    public void VerifySimdConfiguration()
+    {
+        var isAvx2Disabled = Environment.GetEnvironmentVariable("COMPlus_EnableAVX2") == "0";
+        var isSse2Disabled = Environment.GetEnvironmentVariable("COMPlus_EnableSSE2") == "0";
+
+        Assert.That(isAvx2Disabled, Is.EqualTo(!Avx2.IsSupported));
+        Assert.That(isSse2Disabled, Is.EqualTo(!Sse2.IsSupported));
+    }
+}
