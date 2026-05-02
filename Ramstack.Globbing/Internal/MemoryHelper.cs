ebbcda5df65ed34fc6f5d37dcff28b6097f037e1
rameel <rameel-b@hotmail.com>
2026-05-02 21:06:18 +0500

    Add XML documentation for MemoryHelper

--- Ramstack.Globbing/Internal/MemoryHelper.cs
+++ Ramstack.Globbing/Internal/MemoryHelper.cs
@@ -1,12 +1,25 @@
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.Arm;
 using System.Runtime.Intrinsics.X86;
 
 namespace Ramstack.Globbing.Internal;
 
+/// <summary>
+/// Provides low-level memory scanning helpers for UTF-16 character buffers.
+/// </summary>
 internal static unsafe class MemoryHelper
 {
+    /// <summary>
+    /// Searches for the first occurrence of a character in a UTF-16 buffer.
+    /// </summary>
+    /// <param name="s">Pointer to the start of the buffer.</param>
+    /// <param name="e">Pointer to one-past-the-end of the buffer.</param>
+    /// <param name="ch">The character to search for.</param>
+    /// <returns>
+    /// The zero-based index of the first occurrence of <paramref name="ch"/>, or <c>-1</c> if not found.
+    /// </returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int IndexOf(char* s, char* e, char ch)
     {
@@ -85,6 +98,16 @@ internal static unsafe class MemoryHelper
         return -1;
     }
 
+    /// <summary>
+    /// Searches for the first occurrence of either of two characters in a UTF-16 buffer.
+    /// </summary>
+    /// <param name="s">Pointer to the start of the buffer.</param>
+    /// <param name="e">Pointer to one-past-the-end of the buffer.</param>
+    /// <param name="ch1">First character to search for.</param>
+    /// <param name="ch2">Second character to search for.</param>
+    /// <returns>
+    /// The zero-based index of the first occurrence of either character, or <c>-1</c> if neither is found.
+    /// </returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int IndexOfAny(char* s, char* e, char ch1, char ch2)
     {
@@ -166,6 +189,13 @@ internal static unsafe class MemoryHelper
         return -1;
     }
 
+    /// <summary>
+    /// Extracts a bitmask representing the most significant bits of each 16-bit lane in a SIMD vector.
+    /// </summary>
+    /// <param name="v">Input vector of 16-bit integers.</param>
+    /// <returns>
+    /// A bitmask encoding which lanes have their high bit set.
+    /// </returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static int AdvSimd_ExtractMostSignificantBits(Vector128<short> v)
     {
@@ -176,6 +206,13 @@ internal static unsafe class MemoryHelper
         return sum.ToScalar();
     }
 
+    /// <summary>
+    /// Loads a 128-bit vector of UTF-16 characters.
+    /// </summary>
+    /// <param name="source">Pointer to the source memory location.</param>
+    /// <returns>
+    /// A vector containing 8 UTF-16 characters.
+    /// </returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static Vector128<short> LoadVector(void* source) =>
         Unsafe.ReadUnaligned<Vector128<short>>(source);
